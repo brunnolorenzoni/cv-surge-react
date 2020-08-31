11fa436698fbf0523fbbea86a086e02afe89d0ce
Brunno Lorenzoni <lorenzoni.brunno@gmail.com>
2020-08-31 18:39:29 -0300

    Iniciando projeto

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        Desenvolvido com React, Material UI & Coffee
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
---
+++ src/components/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <>
+      <main>
+        <h2>Welcome!</h2>
+      </main>
+    </>
+  );
+}
+
+export default Main;
